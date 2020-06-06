0bfef85694a49aadc61c45cb58e08c71e455d62f
michael <michael@local.com>
2020-06-06 09:01:18 -0700

    Added duplication and access control

--- config/api-config.js
+++ config/api-config.js
@@ -2,7 +2,7 @@ import {SiteConfig} from "./site-config";
 
 export default
 {
-    "apiUrl": "https://fetchertest.truvoicer.co.uk/",
+    "apiUrl": "https://fetchertest.truvoicer.co.uk",
     "endpoints": {
         "login": "/api/login",
         "getUserList": "/api/admin/users",
