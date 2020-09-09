de8aeb9add4f927d1b3866a1aad007fde68149d0
truvoice <miky@local.com>
2020-09-09 05:54:58 +0100

    form refactoring

--- views/components/Forms/RequestResponseKeysForm.js
+++ views/components/Forms/RequestResponseKeysForm.js
@@ -3,7 +3,6 @@ import {fetchData, responseHandler, sendData} from "../../../library/api/middlew
 import React, {useEffect, useState} from "react";
 import {isSet, uCaseFirst} from "../../../library/utils";
 import DataForm from "./DataForm";
-import {ServiceRequestConfigFormData} from "../../../library/forms/service-request-config-form";
 import {ServiceRequestResponseKeysFormData} from "../../../library/forms/service-request-response-keys-form";
 
 const sprintf = require("sprintf-js").sprintf;
@@ -115,65 +114,6 @@ const RequestResponseKeysForm = (props) => {
             }
         </>
     );
-    // render() {
-    //     return (
-    //         <Form onSubmit={this.formSubmitHandler}>
-    //             <Form.Group>
-    //                 <Form.Label>{this.state.key_name}</Form.Label>
-    //                 <Form.Control
-    //                     name={"key_value"}
-    //                     value={this.state.key_value}
-    //                     onChange={this.formChangeHandler}/>
-    //             </Form.Group>
-    //                 <Form.Group controlId="formShowInResponseCheckbox">
-    //                     <Form.Check
-    //                         checked={this.state.show_in_response ? "checked" : ""}
-    //                         type={"checkbox"}
-    //                         id={"show_in_response"}
-    //                         label={"Show in Response?"}
-    //                         name="show_in_response"
-    //                         onChange={this.formChangeHandler}
-    //                     />
-    //                 </Form.Group>
-    //                 <Form.Group controlId="formListItemCheckbox">
-    //                     <Form.Check
-    //                         checked={this.state.list_item ? "checked" : ""}
-    //                         type={"checkbox"}
-    //                         id={"list_item"}
-    //                         label={"List Item?"}
-    //                         name="list_item"
-    //                         onChange={this.formChangeHandler}
-    //                     />
-    //                 </Form.Group>
-    //                 <Form.Group controlId="formHasArrayValueCheckbox">
-    //                     <Form.Check
-    //                         checked={this.state.has_array_value ? "checked" : ""}
-    //                         type={"checkbox"}
-    //                         id={"has_array_value"}
-    //                         label={"Has Array Value?"}
-    //                         name="has_array_value"
-    //                         onChange={this.formChangeHandler}
-    //                     />
-    //                 </Form.Group>
-    //                 {this.state.has_array_value &&
-    //                 <>
-    //                     <Form.Group>
-    //                         <Form.Label>Return Data Type</Form.Label>
-    //                         <Select
-    //                             value={this.state.return_data_type_select}
-    //                             options={this.state.return_data_type_options}
-    //                             onChange={this.formSelectHandler}
-    //                         />
-    //                     </Form.Group>
-    //                     <FormList callback={this.formListCallback} data={this.state.array_keys}/>
-    //                 </>
-    //                 }
-    //                 <Button variant="primary" type="submit">
-    //                     Update
-    //                 </Button>
-    //         </Form>
-    // )
-    // }
 }
 
     export default RequestResponseKeysForm;
